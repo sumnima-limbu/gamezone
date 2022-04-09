6016bbcd2efaf2a9337a504d4edeb7f9d000d86a
sumnima-limbu <sumnimaklimbu@gmail.com>
2022-04-09 13:40:44 +0545

    add global styles

---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 25,
+    padding: 24,
+  },
+  titleText: {
+    fontFamily: "nunito-bold",
+    fontSize: 18,
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+});
